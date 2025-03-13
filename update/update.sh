c0b7be3cce53d25b092ee4e9fe15ea40a0e8e48c
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2025-03-12 20:29:47 -0400

    Add Makejails, Update README, Add templates

---
+++ update/update.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+BASEDIR=`dirname -- "$0"` || exit $?
+BASEDIR=`realpath -- "${BASEDIR}"` || exit $?
+
+. "${BASEDIR}/update.conf"
+
+set -xe
+set -o pipefail
+
+cat -- "${BASEDIR}/Makejail.template" |\
+    sed -Ee "s/%%TAG1%%/${TAG1}/g" > "${BASEDIR}/../Makejail"
+
+cat -- "${BASEDIR}/build.makejail.template" |\
+    sed -E \
+        -e "s/%%VERSION%%/${VERSION}/g" \
+        -e "s/%%GO_VERSION%%/${GO_VERSION}/g" \
+        -e "s/%%NODE_VERSION%%/${NODE_VERSION}/g" > "${BASEDIR}/../build.makejail"
+
+cat -- "${BASEDIR}/README.md.template" |\
+    sed -E \
+        -e "s/%%TAG1%%/${TAG1}/g" \
+        -e "s/%%TAG2%%/${TAG2}/g" \
+        -e "s/%%VERSION%%/${VERSION}/g" > "${BASEDIR}/../README.md"
